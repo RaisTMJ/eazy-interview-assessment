d74d380377f53fd9d089fabf421e410cd21dd961
Mohamad Rais <46097877+RaisTMJ@users.noreply.github.com>
2025-10-06 17:07:56 +0800

    remove middleware logic
    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,14 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    console.log('Middleware running for:', request.url);
-  const token = request.cookies.get("auth_token")?.value;
+  //   console.log('Middleware running for:', request.url);
+  // const token = request.cookies.get("auth_token")?.value;
 
-  // If user not logged in, redirect to login page
-  console.log('Auth Token:', token);
-  if (!token && request.nextUrl.pathname.startsWith("/profile")) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // // If user not logged in, redirect to login page
+  // console.log('Auth Token:', token);
+  // if (!token && request.nextUrl.pathname.startsWith("/profile")) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
   return NextResponse.next();
 }
